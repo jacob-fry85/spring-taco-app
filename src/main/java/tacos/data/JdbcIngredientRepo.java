0f343d2869b941f1084746b7c882cbae2004be29
darwintirta <darwin.hermawan@gmail.com>
2025-11-07 14:56:54 +0700

    update JdbcIngredientRepo

--- src/main/java/tacos/data/JdbcIngredientRepo.java
+++ src/main/java/tacos/data/JdbcIngredientRepo.java
@@ -4,6 +4,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import tacos.Ingredient;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,12 +14,28 @@ public class JdbcIngredientRepo implements IngredientRepo{
     private JdbcTemplate jdbcTemplate;
     @Override
     public Iterable<Ingredient> findAll() {
-        return null;
+        return jdbcTemplate.query(
+                "select id, name, type from Ingredient",
+                this::mapRowToIngredient);
     }
 
     @Override
     public Optional<Ingredient> findById(String id) {
-        return Optional.empty();
+        List<Ingredient> results = jdbcTemplate.query(
+                "select id, name, type from Ingredient where id=?",
+                this::mapRowToIngredient,
+                id);
+        return results.size() == 0 ?
+                Optional.empty() :
+                Optional.of(results.get(0));
+    }
+
+    private Ingredient mapRowToIngredient(ResultSet row, int rowNum) throws SQLException {
+        return new Ingredient(
+                row.getString("id"),
+                row.getString("name"),
+                Ingredient.Type.valueOf((row.getString("type")));
+        )
     }
 
     @Override
