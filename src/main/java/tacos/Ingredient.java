d6cb9009dd3a106609c26b4ae0a3d15cdffae46e
darwintirta <darwin.hermawan@gmail.com>
2025-12-02 21:55:40 +0700

    use cassandra annotation in Ingredient.java

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -5,17 +5,16 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.annotation.Id;
-import tacos.data.IngredientRepo;
+
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
 
 @Data
-@Entity
 @AllArgsConstructor
 @NoArgsConstructor(access= AccessLevel.PRIVATE,force = true)
+@Table("ingredients")
 public class Ingredient {
-    @Id
+    @PrimaryKey
     private final String id;
     private final String name;
     private final Type type;
