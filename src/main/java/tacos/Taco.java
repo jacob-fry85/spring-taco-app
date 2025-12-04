554e05f4f5bdc61b9028bfe76a6674ebb8887d2f
darwintirta <darwin.hermawan@gmail.com>
2025-12-04 23:25:43 +0700

    add IngredientUDT, Taco, TacoURDRUtils, TacoUDT .java

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,30 +1,37 @@
 package tacos;
 
+import com.datastax.oss.driver.api.core.uuid.Uuids;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
+import org.springframework.data.cassandra.core.cql.Ordering;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Data
-@Table
+@Table("tacos")
 public class Taco {
-    @Id
-    private Long id;
+    @PrimaryKeyColumn(type=PrimaryKeyType.PARTITIONED)
+    private UUID id = Uuids.timeBased();
+
+    @PrimaryKeyColumn(type=PrimaryKeyType.CLUSTERED, ordering=Ordering.DESCENDING)
     private Date createdAt = new Date();
 
     @NotNull
     @Size(min=5, message="Name must beat least 5 characters long.")
     private String name;
 
-    @NotNull
+    @Column("ingredients")
     @Size(min=1, message="You must choose at least 1 ingredient")
-    private List<Ingredient> ingredients;
+    private List<IngredientUDT> ingredients;
 
     public void addIngredient(Ingredient ingredient) {
-        this.ingredients.add(ingredient);
+        this.ingredients.add(TacoUDRUtils.toIngredientUDT((ingredient)));
     }
 }
