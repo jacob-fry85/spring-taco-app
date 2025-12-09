4cd58a4e03154c302c9e404eae4efe866c2efce6
darwintirta <darwin.hermawan@gmail.com>
2025-12-10 06:48:16 +0700

    remove final from variable Ingredient, update SecurityConfig

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -14,9 +14,9 @@ import org.springframework.data.cassandra.core.mapping.Table;
 @Table("ingredients")
 public class Ingredient {
     @PrimaryKey
-    private final String id;
-    private final String name;
-    private final Type type;
+    private String id;
+    private String name;
+    private Type type;
 
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/design", "/orders").authenticated()
+                        .requestMatchers("/design").authenticated()
                         .anyRequest().permitAll()
                 )
                 .formLogin(Customizer.withDefaults())  // Enables login form
