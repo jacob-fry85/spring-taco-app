2ad0d76ef0d6c6adbb515a16d36336ac0364c11d
darwintirta <darwin.hermawan@gmail.com>
2025-12-17 23:31:09 +0700

    add RegistrationController.java and UserRepo.java

---
+++ src/main/java/tacos/controller/RegistrationController.java
@@ -0,0 +1,32 @@
+package tacos.controller;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tacos.data.UserRepo;
+
+@Controller
+@RequestMapping("/register")
+public class RegistrationController {
+
+    private UserRepo userRepo;
+    private PasswordEncoder passwordEncoder;
+
+    public RegistrationController(UserRepo userRepo, PasswordEncoder passwordEncoder) {
+        this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping
+    public String registerForm() {
+        return "registration";
+    }
+
+    @PostMapping
+    public String processRegistration(RegistrationForm form) {
+        userRepo.save(form.toUser(passwordEncoder));
+        return "redirect:/login";
+    }
+}
--- src/main/java/tacos/data/UserRepo.java
+++ src/main/java/tacos/data/UserRepo.java
@@ -1,9 +1,6 @@
 package tacos.data;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import tacos.User;
 
 public interface UserRepo extends CrudRepository<User, Long> {
