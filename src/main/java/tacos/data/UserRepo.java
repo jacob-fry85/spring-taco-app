2f75fdcc6a375e5b0437daa2f5fcd2657f005dcc
darwintirta <darwin.hermawan@gmail.com>
2025-12-17 14:48:45 +0700

    update securityconfig and UserRepo

--- src/main/java/tacos/data/UserRepo.java
+++ src/main/java/tacos/data/UserRepo.java
@@ -1,8 +1,11 @@
 package tacos.data;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import tacos.User;
 
 public interface UserRepo extends CrudRepository<User, Long> {
-    User findByUsernam(String username);
+    User findByUsername(String username);
 }
--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import tacos.User;
+import tacos.data.UserRepo;
 
 @Configuration
 public class SecurityConfig {
@@ -28,15 +28,25 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-                .username("darwin")
-                .password("{noop}password") // {noop} means no password encoder
-                .roles("USER")
-                .build();
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user = User.builder()
+//                .username("darwin")
+//                .password("{noop}password") // {noop} means no password encoder
+//                .roles("USER")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
 
-        return new InMemoryUserDetailsManager(user);
+    @Bean
+    public UserDetailsService userDetailsService(UserRepo userRepo) {
+        return username -> {
+            User user = userRepo.findByUsername(username);
+            if(user == null) return user;
+
+            throw new UsernameNotFoundException("User '" + username + "' not found");
+        };
     }
 
     @Bean
